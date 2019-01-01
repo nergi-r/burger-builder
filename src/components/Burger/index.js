6c3da0c415271c4fee83e313fdbba435c2cc9741
nergi-r <nergi.r@gmail.com>
2019-01-02 01:02:59 +0700

    Added some mock data to see burger ingredients displayed.

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -4,11 +4,17 @@ import classes from './index.module.css';
 import BurgerIngrendient from './BurgerIngredient';
 
 const burger = (props) => {
+  const transformedIngredient = Object.keys(props.ingredients)
+    .map(ingredientKey => {
+      return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
+        return <BurgerIngrendient key={ingredientKey + i} type={ingredientKey} />;
+      });
+    });
+
   return (
     <div className={classes.Burger}>
       <BurgerIngrendient type="bread-top" />
-      <BurgerIngrendient type="cheese" />
-      <BurgerIngrendient type="meat" />
+      {transformedIngredient}
       <BurgerIngrendient type="bread-bottom" />
     </div>
   );
