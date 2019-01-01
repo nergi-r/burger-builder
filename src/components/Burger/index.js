2aae16e9b19c738b65725f7db6b394e2e9bb722e
nergi-r <nergi.r@gmail.com>
2019-01-02 01:13:46 +0700

    Add validation to empty ingredients.

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -4,12 +4,17 @@ import classes from './index.module.css';
 import BurgerIngrendient from './BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredient = Object.keys(props.ingredients)
+  let transformedIngredient = Object.keys(props.ingredients)
     .map(ingredientKey => {
       return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
         return <BurgerIngrendient key={ingredientKey + i} type={ingredientKey} />;
       });
-    });
+    })
+    .reduce((ingredients, ingredient) => { return ingredients.concat(ingredient) }, []);
+
+  if (transformedIngredient.length === 0) {
+    transformedIngredient = <p>Please start adding ingredients!</p>
+  }
 
   return (
     <div className={classes.Burger}>
